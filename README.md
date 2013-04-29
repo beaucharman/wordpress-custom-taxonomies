a66c6a90fc78209bd1a3b21ed525b567bece286f
Beau Charman <beau.charman@gmail.com>
2013-04-29 23:40:53 +1000

    Readme and code changes. v2

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
-README.md
+# WordPress Custom Taxonomies
+
+A PHP class to help register and maintain WordPress custom taxonomies. It also comes with some rad built-in properties and methods that can be used in templates to maintain clean code and modular development.
+
+For more information about registering taxonomies, including a full list of options, visit the [WordPress Codex](http://codex.wordpress.org/Function_Reference/register_taxonomy).
+
+This class works well with the [WordPress Custom Post Types class](https://github.com/beaucharman/WordPress-Custom-Post-Types).
+
+### Declaring New Taxonomies
+
+Include `taxonomies.php` in your `functions.php` file.
+
+Declare the various argument arrays to setup the new taxonomy as needed (`$name` is required):
+
+```php
+// Required
+$name = '';
+
+// Optional
+// The post type/s that the taxonomy is connected to.
+// String or array
+$post_type = '';
+
+// Optional
+$labels = array(
+  'label_singular'        => '',
+  'label_plural'          => '',
+  'menu_label'            => ''
+ );
+
+// Optional
+$options = array(
+  'public'                => true,
+  'show_ui'               => true,
+  'show_in_nav_menus'     => true,
+  'show_tagcloud'         => true,
+  'hierarchical'          => false,
+  'update_count_callback' => null,
+  'query_var'             => true,
+  'rewrite'               => true,
+  'capabilities'          => array(),
+  'sort'                  => null
+ );
+
+// Optional
+$help = '';
+```
+
+Then create a variable (for future reference, but is not required) from an instance of the LT3_Custom_Taxonomy class:
+
+```php
+$Taxonomy = new LT3_Custom_Taxonomy( $name, $post_type, $labels, $options, $help );
+```
+
+### Usage
+
+The custom taxonomy class creates a handfull of useful properties and methods that can be accessed through the taxonomy's instance variable and can be used in template and admin files.
+
+#### Properties
+
+$Taxonomy->name
+
+The taxonomy slug.
+
+$Taxonomy->lables
+
+A an array of the singular, plural and menu lables.
+
+#### Methods
+
+$Taxonomy->archive_link()
+
+Gets the absolute permalink to the taxonomies's archive page.
+
+$Taxonomy->get()
+
+Get all the terms assigned to this taxonomy. Accepts an array of arguments, and a boolean value to retrieve just a single value (true, useful to use along side 'include' => array( $single_id ) ) or an array of results (false).
+
+For example:
+
+```php
+$taxonomies = $Taxonomy->get();
+```
+
+**Note:** A declaration of `global $Taxonomy;` might be required on some template files.
+
+See the [Get Terms => Parameters](http://codex.wordpress.org/Function_Reference/get_terms#Parameters) codex reference for the list of possible arguments, and the [Get Terms => Return Values](http://codex.wordpress.org/Function_Reference/get_terms#Return_Values) codex reference for the list of return values.
--- custom-taxonomies.php
+++ custom-taxonomies.php
@@ -1,136 +1,207 @@
 <?php
-/*
+/**
+ * Taxonomies
+ * ------------------------------------------------------------------------
+ * taxonomies.php
+ * @version 2.0 | April 1st 2013
+ * @package lt3
+ * @author  Beau Charman | @beaucharman | http://beaucharman.me
+ * @link    https://github.com/beaucharman/WordPress-Custom-Taxonomies
+ * @license MIT license
+ *
+ * Properties
+ *  $Taxonomy->name | sring
+ *  $Taxonomy->lables | array
+ *
+ * Methods
+ *  $Taxonomy->get()
+ *  $Taxonomy->archive_link()
+ *
+ * For more information about registering Taxonomies:
+ * http://codex.wordpress.org/Function_Reference/register_taxonomy
+ *
+ * To declare a custom taxonomy, simply create a new instance of the
+ * LT3_Custom_Taxonomy class.
+ */
 
-  lt3-theme Custom Taxonomies
-
-------------------------------------------------
-  custom-taxonomies.php
-  @version 2.0 | April 1st 2013
-  @package lt3
-  @author  Beau Charman | @beaucharman | http://beaucharman.me
-  @link    https://github.com/beaucharman/lt3
-  @licence GNU http://www.gnu.org/licenses/lgpl.txt
-
-  For more information about registering Taxonomies:
-  http://codex.wordpress.org/Function_Reference/register_taxonomy
-
-  You can also turn the custom post types declarations into a plugin. for more information: http://codex.wordpress.org/Writing_a_Plugin
-
-  To declare a taxonomy, simply add a taxonomy array to the $lt3_custom_taxonomies array variable, with required values of:
-  array(
-    'slug_singuar'          => '',
-    'slug_plural'           => '',
-    'name_singular'         => '',
-    'name_plural'           => '',
-    // and optional values of:
-    'public'                => true,
-    'show_in_nav_menus'     => true,
-    'show_ui'               => true,
-    'show_tagcloud'         => true,
-    'hierarchical'          => true,
-    'update_count_callback' => NULL,
-    'query_var'             => true,
-    'rewrite'               => '',
-    'capabilities'          => array(),
-    'sort'                  => NULL,
-    'post_type'             => array('')
-  )
------------------------------------------------- */
-
-/*
-
-  Declare Taxonomies
-
------------------------------------------------- */
-$lt3_custom_taxonomies = array();
-
-/*
-
-  Register Taxonomies
-
------------------------------------------------- */
-add_action('init', 'lt3_register_taxonomies', 0);
-function lt3_register_taxonomies()
+/* ------------------------------------------------------------------------
+   Custom taxonomy class
+   ------------------------------------------------------------------------ */
+class LT3_Custom_Taxonomy
 {
-  global $lt3_custom_taxonomies;
-  foreach($lt3_custom_taxonomies as $ct)
+  public $name;
+  public $post_type;
+  public $labels;
+  public $options;
+  public $help;
+
+  /* Class constructor */
+  public function __construct( $name, $post_type = array(), $labels = array(), $options = array(), $help = null )
   {
-    $labels = array(
-      'name'                  => __($ct['name_plural'], $ct['name_plural'] . ' general name'),
-      'singular_name'         => __($ct['name_singular'], $ct['name_singular'] . ' singular name'),
-      'search_items'          => __('Search ' . $ct['name_plural']),
-      'all_items'             => __('All ' . $ct['name_plural']),
-      'parent_item'           => __('Parent ' . $ct['name_singular']),
-      'parent_item_colon'     => __('Parent '. $ct['name_singular'] .':'),
-      'edit_item'             => __('Edit '. $ct['name_singular']),
-      'update_item'           => __('Update ' . $ct['name_singular']),
-      'add_new_item'          => __('Add New ' . $ct['name_singular']),
-      'new_item_name'         => __('New ' . $ct['name_singular']),
-      'menu_name'             => __($ct['name_plural'])
-    );
-    register_taxonomy($ct['slug_singular'], $ct['post_type'], array(
-      'labels'                => $labels,
-      'public'                => ($ct['public'])                ? $ct['public'] : true,
-      'show_in_nav_menus'     => ($ct['show_in_nav_menus'])     ? $ct['show_in_nav_menus'] : true,
-      'show_ui'               => ($ct['show_ui'])               ? $ct['show_ui'] : true,
-      'show_tagcloud'         => ($ct['show_tagcloud'])         ? $ct['show_tagcloud'] : true,
-      'show_admin_column'     => ($ct['show_admin_column'])     ? $ct['show_admin_column'] : false,
-      'hierarchical'          => ($ct['hierarchical'])          ? $ct['hierarchical'] : false,
-      'update_count_callback' => ($ct['update_count_callback']) ? $ct['update_count_callback'] : null,
-      'query_var'             => ($ct['query_var'])             ? $ct['query_var'] : $ct['slug_plural'],
-      'rewrite'               => ($ct['rewrite'])               ? $ct['rewrite'] : true,
-      'capabilities'          => ($ct['capabilities'])          ? $ct['capabilities'] : array(),
-      'sort'                  => ($ct['sort'])                  ? $ct['sort'] : null
-    ));
-  }
-}
+    $this->name      = $this->uglify_words( $name );
+    $this->post_type = $post_type;
+    $this->labels    = $labels;
+    $this->options   = $options;
+    $this->help      = $help;
 
-/*
-
-  Create Taxonomy drop downs for all post types
-
------------------------------------------------- */
-add_action('restrict_manage_posts', 'lt3_todo_restrict_manage_posts');
-function lt3_todo_restrict_manage_posts()
-{
-  global $typenow;
-  $args=array('public' => true, '_builtin' => false);
-  $post_types = get_post_types($args);
-  if (in_array($typenow, $post_types))
-  {
-    $filters = get_object_taxonomies($typenow);
-    foreach ($filters as $tax_slug)
+    if ( !taxonomy_exists( $this->name ) )
     {
-      $tax_obj = get_taxonomy($tax_slug);
-      wp_dropdown_categories(array(
-        'show_option_all' => __('Show All '.$tax_obj->label),
-        'taxonomy' => $tax_slug,
-        'name' => $tax_obj->name,
-        'orderby' => 'term_order',
-        'selected' => $_GET[$tax_obj->query_var],
-        'hierarchical' => $tax_obj->hierarchical,
-        'show_count' => false,
-        'hide_empty' => true
-      ));
+      add_action( 'init', array( &$this, 'register_custom_taxonomies' ), 0 );
+      if ( $this->help ) add_action( 'contextual_help', array( &$this, 'add_custom_contextual_help' ), 10, 3 );
     }
   }
-}
 
-add_filter('parse_query','lt3_todo_convert_restrict');
-function lt3_todo_convert_restrict($query)
-{
-  global $pagenow,  $typenow;
-  if ($pagenow=='edit.php')
+  /**
+   * Register Taxonomies
+   * ------------------------------------------------------------------------
+   * register_custom_taxonomies()
+   * @param  null
+   * @return taxonomy
+   * ------------------------------------------------------------------------ */
+  public function register_custom_taxonomies()
   {
-    $filters = get_object_taxonomies($typenow);
-    foreach ($filters as $tax_slug)
+    /* Create the labels */
+    $this->labels['label_singular'] = ( isset( $this->labels['label_singular'] ) )
+      ? $this->labels['label_singular'] : $this->prettify_words( $this->name );
+    $this->labels['label_plural'] = ( isset( $this->labels['label_plural'] ) )
+      ? $this->labels['label_plural'] : $this->plurafy_words( $this->labels['label_singular'] );
+    $this->labels['menu_label'] = ( isset( $this->labels['menu_label'] ) )
+      ? $this->labels['menu_label'] : $this->labels['label_plural'];
+
+    $labels = array(
+      'name'                  => __( $this->labels['label_plural'], $this->labels['label_plural'] . ' general name' ),
+      'singular_name'         => __( $this->labels['label_singular'], $this->labels['label_singular'] . ' singular name' ),
+      'menu_name'             => __( $this->labels['menu_label'] ),
+      'search_items'          => __( 'Search ' . $this->labels['label_plural'] ),
+      'all_items'             => __( 'All ' . $this->labels['label_plural'] ),
+      'parent_item'           => __( 'Parent ' . $this->labels['label_singular'] ),
+      'parent_item_colon'     => __( 'Parent '. $this->labels['label_singular'] . ':' ),
+      'edit_item'             => __( 'Edit ' . $this->labels['label_singular'] ),
+      'update_item'           => __( 'Update ' . $this->labels['label_singular'] ),
+      'add_new_item'          => __( 'Add New ' . $this->labels['label_singular'] ),
+      'new_item_name'         => __( 'New ' . $this->labels['label_singular'] ),
+
+     );
+
+    /* Configure the options */
+    $options = array_merge(
+      array(
+        'labels'                => $labels,
+        'hierarchical'          => true,
+        'query_var'             => $this->name,
+        'rewrite'               => true
+       ),
+      $this->options
+     );
+
+    /* Register the new taxonomy */
+    register_taxonomy( $this->name, $this->post_type, $options );
+  }
+
+  /**
+   * Add custom contextual help
+   * ------------------------------------------------------------------------
+   * add_custom_contextual_help()
+   * ------------------------------------------------------------------------ */
+  public function add_custom_contextual_help( $contextual_help, $screen_id, $screen )
+  {
+    $context = 'edit-' . $this->name;
+    if ( $context == $screen->id )
     {
-      $var = &$query->query_vars[$tax_slug];
-      if (isset($var))
-      {
-        $term = get_term_by('id',$var,$tax_slug);
-        $var = $term->slug;
-      }
+      $contextual_help = $this->help;
+    }
+    return $contextual_help;
+  }
+
+  /**
+   * Get
+   * ------------------------------------------------------------------------
+   * get()
+   * @param  $user_args | array
+   * @return term data
+   * ------------------------------------------------------------------------ */
+  public function get( $user_args = array(), $single = false )
+  {
+    $args = array_merge(
+      array(
+        'orderby'       => 'name',
+        'order'         => 'ASC',
+        'hide_empty'    => false
+      ), $user_args
+    );
+    if ( $single )
+    {
+      $items = get_terms( $this->name, $args );
+      return $items[0];
+    }
+    return get_terms( $this->name, $args );
+  }
+
+  /**
+   * Archive Link
+   * ------------------------------------------------------------------------
+   * archive_link()
+   * @param  none
+   * @return string
+   * ------------------------------------------------------------------------ */
+  public function archive_link()
+  {
+    return home_url('/'.$this->name);
+  }
+
+  /**
+   * Prettify words
+   * ------------------------------------------------------------------------
+   * prettify_words()
+   * @param  $words | string
+   * @return string
+   *
+   * Creates a pretty version of a string, like
+   * a pug version of a dog.
+   * ------------------------------------------------------------------------ */
+  public function prettify_words( $words )
+  {
+    return ucwords( str_replace( '_', ' ', $words ) );
+  }
+
+  /**
+   * Uglify words
+   * ------------------------------------------------------------------------
+   * uglify_words()
+   * @param  $words | string
+   * @return string
+   *
+   * creates a url firendly version of the given string.
+   * ------------------------------------------------------------------------ */
+  public function uglify_words( $words )
+  {
+    return strToLower( str_replace( ' ', '_', $words ) );
+  }
+
+  /**
+   * Plurify words
+   * ------------------------------------------------------------------------
+   * plurafy_words()
+   * @param  $words | string
+   * @return $words | string
+   *
+   * Plurifies most common words. Not currently working
+   * proper nouns, or more complex words, for example
+   * knife -> knives, leaf -> leaves.
+   * ------------------------------------------------------------------------ */
+  public function plurafy_words( $words )
+  {
+    if ( strToLower( substr( $words, -1 ) ) == 'y' )
+    {
+      return substr_replace( $words, 'ies', -1 );
+    }
+    if ( strToLower( substr( $words, -1 ) ) == 's' )
+    {
+      return $words . 'es';
+    }
+    else
+    {
+      return $words . 's';
     }
   }
 }
